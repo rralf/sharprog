4e3d32112f7022646bf2fe53bf448fa4e044f134
Ralf Ramsauer <ralf@vmexit.de>
2020-09-29 01:30:32 +0200

    add a RS232 protocol header
    Signed-off-by: Ralf Ramsauer <ralf@vmexit.de>

---
+++ protocol.h
@@ -0,0 +1,17 @@
+/*
+ * sharprog - An AVR programmer for SHARP Pocket Computers
+ *
+ * Copyright (c) Ralf Ramsauer, 2020
+ *
+ * Authors:
+ *  Ralf Ramsauer <ralf@vmexit.de>
+ *
+ * This work is licensed under the terms of the GNU GPL, version 2.  See
+ * the COPYING file in the top-level directory.
+ */
+
+#define RS232_CMD_PING	'p'
+#define RS232_CMD_TAP	't'
+
+#define RS232_FAIL	'E'
+#define RS232_SUCCESS	'X'
